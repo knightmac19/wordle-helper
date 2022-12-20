8ee5d7f16ecad933b9d76bdb9d6258cdcb20ed61
Patrick Dunn <pmdunn78@gmail.com>
2022-12-20 13:35:51 -0500

    basic header footer and page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  const icons = [
+    {
+      name: "fab fa-github",
+      link: "https://github.com/"
+    },
+    {
+      name: "fab fa-linkedin",
+      link: "https://www.linkedin.com/"
+    }
+  ]
+  
+  return (
+    <footer>
+      <p>Patrick Dunn</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Page/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page(props) {
+  return (
+    <div>
+      This is where content will go
+    </div>
+  )
+}
+
+export default Page;
